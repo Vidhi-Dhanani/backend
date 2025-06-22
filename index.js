48d4f90d5adddc42c6d8628b12fa81173a0cda49
VidhiDhanani-1607 <202201076@daiict.ac.in>
2025-06-22 16:53:02 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,20 @@
+require('dotenv').config()
+const express = require('express')
+const app = express()
+const port = 3000
+
+app.get('/', (req, res) => {
+  res.send('Hello Vidhi!')
+})
+
+app.get('/twitter', (req, res) => {
+  res.send('Hello Manav!')
+})
+
+app.get('/login', (req, res) => {
+  res.send('<h1> Hello world!!')
+})
+
+app.listen(process.env.PORT, () => {
+  console.log(`Example app listening on port ${port}`)
+})
